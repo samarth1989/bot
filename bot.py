a3e23ed365b8b286fa0b2db5af2961048051eb43
samarth <30378006+samarth1989@users.noreply.github.com>
2021-04-26 16:52:08 +0100

    Revert "Merge 'origin/main' into recommiting-on-new-branch"
    This reverts commit 49499293db8be20b1ede684217907cb1d5325698, reversing
    changes made to 478cf15b666d3b2ff687df19ee6b806dc72c5280.

--- bot.py
+++ bot.py
@@ -5,27 +5,36 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 app = Flask(__name__) 
 english_bot = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
+#trainer.train("chatterbot.corpus.english")
 trainer.train("data/data.yml")
 
 
 @app.route("/")
 def index():
-     return render_template("chat.html") #to send context to html
+     return render_template("home.html") #to send context to html
+
+@app.route("/filter")
+def filter():
+     type_1=request.args.get("type",default="rent",type=str)
+     price=request.args.get("price",default="rent",type=str)
+     return render_template("filterhome.html") #to send context to html
 
 @app.route("/get")
 def get_bot_response():
      userText = request.args.get("msg") #get data from input,we write js  to index.html
-     #i=0
-     #resp = []
-     #while i < 3:
-     #resp.append(english_bot.get_response(userText))
-          #i+=1
-     return str(english_bot.get_response(userText))
-     #new_menu = list(dict.fromkeys(resp))
-     #return resp
-     
+     txt=english_bot.get_response(userText)
+     return str(txt)
+
+@app.route("/details")
+def detailspage():
+     id=request.args.get("id",default="1",type=str)
+     return render_template('housedetails.html') 
+
+@app.route("/appointment")
+def appointmentpage():
+     id=request.args.get("id",default="1",type=str)
+     return render_template('appointment.html')           
+
 if __name__ == "__main__":
-     app.run(debug = True)
-
-
+     app.run()
+ 
--- sellandlethouses/README.md
+++
@@ -1,8 +0,0 @@
-# sellandlethouses
-A lite weight website for selling and letting houses
-
-Website under development by -
-
-Samarth Rout
-Jithin Thomas
-Nelson Morris
